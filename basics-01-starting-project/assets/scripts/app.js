42d548b205659e156b1966107339e2473ce87dcc
VrajPatel3182 <vrajpatel3102vp@gmail.com>
2023-05-18 12:52:23 +0530

    First commit of calculator

---
+++ basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,10 @@
+const defaultResults = 0;
+let currentResults = defaultResults;
+
+currentResults = (currentResults + 10) * 3 / 2 - 1;
+
+let calculationDescription = `(${defaultResults} + 10) *3 / 2 -1)`;
+
+outputResult(currentResults, calculationDescription);
+
+function
