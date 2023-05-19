a26dae45910a925c9d0651a761c8bddbb5305b61
VrajPatel3182 <vrajpatel3102vp@gmail.com>
2023-05-19 14:45:26 +0530

    sort way to calculate the variable

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,40 +1,42 @@
 const defaultResults = 0;
 let currentResults = defaultResults;
 
+//Gets input from input fields
 function getUserInput(){
   return parseInt(userInput.value);
 }
 
+//Generates and writes calculation logs
 function createAndWriteLog(operator,resultBeforeCalc, calcNumber){
   const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-  outputResult(currentResults,calcDescription);
+  outputResult(currentResults,calcDescription);//from vender file
 }
 
 function add(){
   const enteredNumber = getUserInput();
   const initialResult = currentResults;
-  currentResults = currentResults + enteredNumber;
+  currentResults += enteredNumber;
   createAndWriteLog('+', initialResult, enteredNumber);
 }
 
 function subtract(){
   const enteredNumber = getUserInput();
   const initialResult = currentResults;
-  currentResults = currentResults - enteredNumber;
+  currentResults -= enteredNumber;
   createAndWriteLog('-', initialResult, enteredNumber);
 }
 
 function multiply(){
   const enteredNumber = getUserInput();
   const initialResult = currentResults;
-  currentResults = currentResults * enteredNumber;
+  currentResults *= enteredNumber;
   createAndWriteLog('*', initialResult, enteredNumber);
 }
 
 function divide(){
   const enteredNumber = getUserInput();
   const initialResult = currentResults;
-  currentResults = currentResults / enteredNumber;
+  currentResults /= enteredNumber;
   createAndWriteLog('/', initialResult, enteredNumber);
 }
 
