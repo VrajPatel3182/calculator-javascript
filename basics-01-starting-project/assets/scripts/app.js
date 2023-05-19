7c741f0d43e7e87c04c203fc489ce35e2cffe817
VrajPatel3182 <vrajpatel3102vp@gmail.com>
2023-05-19 14:22:05 +0530

    connecting all buttons to the functions

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,10 +1,45 @@
 const defaultResults = 0;
 let currentResults = defaultResults;
 
-function add(num1, num2){
-  currentResults = currentResults + userInput.value;
-  outputResult(currentResults, '');
+function getUserInput(){
+  return parseInt(userInput.value);
+}
+
+function createAndWriteLog(operator,resultBeforeCalc, calcNumber){
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResults,calcDescription);
+}
+
+function add(){
+  const enteredNumber = getUserInput();
+  const initialResult = currentResults;
+  currentResults = currentResults + enteredNumber;
+  createAndWriteLog('+', initialResult, enteredNumber);
+}
+
+function subtract(){
+  const enteredNumber = getUserInput();
+  const initialResult = currentResults;
+  currentResults = currentResults - enteredNumber;
+  createAndWriteLog('-', initialResult, enteredNumber);
+}
+
+function multiply(){
+  const enteredNumber = getUserInput();
+  const initialResult = currentResults;
+  currentResults = currentResults * enteredNumber;
+  createAndWriteLog('*', initialResult, enteredNumber);
+}
+
+function divide(){
+  const enteredNumber = getUserInput();
+  const initialResult = currentResults;
+  currentResults = currentResults / enteredNumber;
+  createAndWriteLog('/', initialResult, enteredNumber);
 }
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
 
